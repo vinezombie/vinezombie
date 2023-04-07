ee5f72d5234bbfe3501e52f88c26cf66300f193e
TheDaemoness <she@daemoness.dev>
2023-04-07 08:21:54 -0700

    Test some of the string subtypes

--- src/string/subtypes.rs
+++ src/string/subtypes.rs
@@ -1,7 +1,7 @@
 #![allow(clippy::derivable_impls)]
 
-use std::{borrow::Borrow, num::NonZeroUsize};
 use super::{Bytes, Transform};
+use std::{borrow::Borrow, num::NonZeroUsize};
 
 macro_rules! impl_subtype {
     (
@@ -215,7 +215,7 @@ fn user_byte_check(byte: &u8) -> bool {
 }
 
 impl_subtype! {
-    "An [`Arg`] that does not contain any of `!` or `@`.\nIntended for use with nicknames."
+    "An [`Arg`] that does not contain `!` or `@`.\nIntended for use with nicknames."
     Nick: Arg
     NickSafe: ArgSafe
     |bytes| {
@@ -227,7 +227,7 @@ impl_subtype! {
 }
 
 impl_subtype! {
-    "An [`Arg`] that does not contain any of `@` or `%`.\nIntended for use with usernames."
+    "An [`Arg`] that does not contain `@` or `%`.\nIntended for use with usernames."
     User: Arg
     UserSafe: ArgSafe
     |bytes| {
@@ -298,5 +298,39 @@ impl std::error::Error for InvalidByte {}
 impl From<InvalidByte> for std::io::Error {
     fn from(value: InvalidByte) -> Self {
         std::io::Error::new(std::io::ErrorKind::InvalidData, value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Line, Word, Arg};
+
+    #[test]
+    pub fn line() {
+        assert!(Line::from_bytes("foo bar").is_ok());
+        assert!(Line::from_bytes("").is_ok());
+        assert!(Line::from_bytes("foobar\n").is_err());
+        assert!(Line::from_bytes("foo\nbar").is_err());
+        assert!(Line::from_bytes("foobar\r\n").is_err());
+        assert!(Line::from_bytes("foobar\r").is_err());
+        assert!(Line::from_bytes("foo\rbar").is_err());
+    }
+
+    #[test]
+    pub fn word() {
+        assert!(Word::from_bytes("foobar").is_ok());
+        assert!(Word::from_bytes("").is_ok());
+        assert!(Word::from_bytes("foo\nbar").is_err());
+        assert!(Word::from_bytes("foo bar").is_err());
+        assert!(Word::from_bytes("foobar ").is_err());
+        assert!(Word::from_bytes(" foobar").is_err());
+    }
+
+    #[test]
+    pub fn arg() {
+        assert!(Arg::from_bytes("foobar").is_ok());
+        assert!(Arg::from_bytes("foo:bar").is_ok());
+        assert!(Arg::from_bytes("").is_err());
+        assert!(Arg::from_bytes(":foo").is_err());
     }
 }
