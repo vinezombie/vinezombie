2bb6a0d5786019ec871ab6ee60ddbe16ac0c6110
TheDaemoness <she@daemoness.dev>
2023-06-10 11:29:37 -0700

    Update state.rs

--- src/state.rs
+++ src/state.rs
@@ -1,8 +1,7 @@
 //! Representations of IRC network and connection state, including users and channels.
 
-use crate::known::{Cap, MaybeKnown};
-use crate::msg::data::CapStatus;
-use crate::IrcWord;
+use crate::string::{Arg, Key, Nick, Word};
+use std::borrow::Borrow;
 use std::collections::{BTreeMap, BTreeSet};
 use std::sync::{Arc, RwLock};
 
@@ -11,33 +10,42 @@ use std::sync::{Arc, RwLock};
 pub struct Network {
     // TODO: Channel lists, user lists.
     /// The capabilities that are available on this network.
-    pub caps: BTreeSet<MaybeKnown<'static, Cap>>,
+    pub caps: BTreeSet<Arg<'static>>,
     /// The values of some capabilities that are available on this network.
     ///
     /// Presence of a key in this map does not imply that a capability is available,
     /// that it has the specified value if it is available.
     /// You should ideally ensure that the set of keys is a subset of [Network::caps].
-    pub caps_values: BTreeMap<MaybeKnown<'static, Cap>, IrcWord<'static>>,
+    pub caps_values: BTreeMap<Arg<'static>, Word<'static>>,
 }
 
 impl Network {
     /// Adds a capability.
-    pub fn cap_add(
-        &mut self,
-        cap: impl Into<MaybeKnown<'static, Cap>>,
-        value: Option<IrcWord<'static>>,
-    ) {
-        let cap = cap.into();
-        if let Some(value) = value {
+    pub fn cap_add(&mut self, cap: Arg<'static>, value: Word<'static>) {
+        if !value.is_empty() {
             self.caps_values.insert(cap.clone(), value);
         }
         self.caps.insert(cap);
     }
+    /// Looks up a capability, returning its value if it exists.
+    pub fn cap_get<K>(&mut self, cap: &K) -> Option<Word<'static>>
+    where
+        K: Ord + ?Sized,
+        Arg<'static>: Borrow<K>,
+    {
+        if !self.caps.contains(cap) {
+            return None;
+        }
+        Some(self.caps_values.get(cap).cloned().unwrap_or_default())
+    }
     /// Removes a capability.
-    pub fn cap_del(&mut self, cap: impl Into<MaybeKnown<'static, Cap>>) {
-        let cap = cap.into();
-        self.caps_values.remove(&cap);
-        self.caps.remove(&cap);
+    pub fn cap_del<K>(&mut self, cap: &K)
+    where
+        K: Ord + ?Sized,
+        Arg<'static>: Borrow<K>,
+    {
+        self.caps_values.remove(cap);
+        self.caps.remove(cap);
     }
 }
 
@@ -45,40 +53,16 @@ impl Network {
 #[derive(Clone, Debug)]
 pub struct Connection {
     /// The nickname currently in use.
-    pub nick: IrcWord<'static>,
+    pub nick: Nick<'static>,
     /// The capabilities that have been enabled for this connection.
-    pub caps: BTreeSet<MaybeKnown<'static, Cap>>,
+    pub caps: BTreeSet<Key<'static>>,
     /// The state of the network.
     pub net: Arc<RwLock<Network>>,
 }
 
 impl Connection {
     /// Creates a new connection state tracker using the given nick and shared network state.
-    pub fn new(nick: IrcWord<'static>, net: Option<Arc<RwLock<Network>>>) -> Self {
+    pub fn new(nick: Nick<'static>, net: Option<Arc<RwLock<Network>>>) -> Self {
         Connection { nick, caps: BTreeSet::new(), net: net.unwrap_or_default() }
-    }
-    /// Updates the statuses of capabilities.
-    pub fn update_caps(
-        &mut self,
-        iter: impl IntoIterator<Item = (MaybeKnown<'static, Cap>, CapStatus)>,
-    ) {
-        // TODO: Use LazyCell whenever that stabilizes to acquire the net lock lazily.
-        let mut net = self.net.write().unwrap();
-        for (cap, status) in iter {
-            match status {
-                CapStatus::Available(value) => net.cap_add(cap, value),
-                CapStatus::Enabled => {
-                    net.caps.insert(cap.clone());
-                    self.caps.insert(cap);
-                }
-                CapStatus::Unchanged => (),
-                CapStatus::Disabled(avail) => {
-                    self.caps.remove(&cap);
-                    if !avail {
-                        net.cap_del(cap);
-                    }
-                }
-            }
-        }
     }
 }
