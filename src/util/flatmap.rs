7fc48b192a6efbc1d889cae36fe6c3933b251ebc
TheDaemoness <she@daemoness.dev>
2024-04-11 17:51:56 -0700

    Make FlatMapEditGuard safe to forget
    Forgetting the EditGuard could leave the FlatMap in an invalid (but safe)
    state. This should no longer be the case; it now leaves the FlatMap
    in a valid state containing a safe subset of its elements.

--- src/util/flatmap.rs
+++ src/util/flatmap.rs
@@ -58,23 +58,34 @@ impl<E, X: KeyExtractor<E>> Default for FlatMap<E, X> {
     }
 }
 
+/// Darkly magical guard against the inner Vec containing out-of-order elements.
+///
+/// This type abuses Vec::set_len to reassign the meaning of the vector's internal length variable
+/// to mean not the number of initialized units of memory in the vector, but to be one past
+/// the index of the last element that is part of a contiguous sequence of sorted elements
+/// from the start of the Vec. The real length is kept in `real_len` which is always `>=` Vec::len.
+///
+/// This means that if the guard is forgotten,
+/// the FlatMap will still contain only elements in sorted order.
 #[derive(PartialEq, Eq, Hash, Debug)]
 pub struct FlatMapEditGuard<'a, E, X: KeyExtractor<E>> {
     src: &'a mut Vec<E>,
-    sorted_until: usize,
+    real_len: usize,
     marker: std::marker::PhantomData<X>,
 }
 
 impl<'a, E, X: KeyExtractor<E>> Drop for FlatMapEditGuard<'a, E, X> {
     fn drop(&mut self) {
-        if self.sorted_until < self.src.len() {
+        let sorted_until = self.src.len();
+        unsafe { self.src.set_len(self.real_len) };
+        if sorted_until < self.src.len() {
             self.src.sort_unstable_by(|l, r| X::extract_key(l).cmp(X::extract_key(r)));
         }
     }
 }
 
 /// Efficient keyed sequential deduplication that keeps the last duplicate element, not the first.
-fn do_dedup<E, X: KeyExtractor<E>>(vec: Vec<E>) -> Vec<E> {
+pub(super) fn do_dedup<E, X: KeyExtractor<E>>(vec: Vec<E>) -> Vec<E> {
     // What follows uses a lot of unsafe.
     if vec.len() < 2 {
         return vec;
@@ -115,37 +126,6 @@ fn do_dedup<E, X: KeyExtractor<E>>(vec: Vec<E>) -> Vec<E> {
         Vec::from_raw_parts(ptr, new_len, cap)
     }
 }
-
-#[test]
-fn test_dedup() {
-    let testcases = [
-        (vec![], [].as_slice()),
-        (vec![(1, 0)], &[(1, 0)]),
-        (vec![(1, 0), (2, 0)], &[(1, 0), (2, 0)]),
-        (vec![(1, 0), (2, 0), (3, 0)], &[(1, 0), (2, 0), (3, 0)]),
-        (vec![(1, 0), (2, 0), (2, 0)], &[(1, 0), (2, 0)]),
-        (vec![(1, 0), (1, 0), (2, 0)], &[(1, 0), (2, 0)]),
-        (vec![(1, 0), (2, 0), (2, 0), (3, 0)], &[(1, 0), (2, 0), (3, 0)]),
-        (vec![(1, 0), (2, 0), (2, 1), (2, 2), (3, 0)], &[(1, 0), (2, 2), (3, 0)]),
-    ];
-    for (init, expected) in testcases {
-        let result = do_dedup::<_, ()>(init);
-        assert_eq!(&result, expected);
-    }
-    // Simple test to hopefully catch UAFs.
-    let vec1 = do_dedup::<_, ()>(vec![
-        (1, String::from("foo")),
-        (1, String::from("bar")),
-        (2, String::from("baz")),
-    ]);
-    let vec2 = do_dedup::<_, ()>(vec![
-        (1, String::from("bar")),
-        (2, String::from("foo")),
-        (2, String::from("baz")),
-    ]);
-    assert_eq!(vec1, vec2);
-}
-
 // TODO: We need more tests for this thing.
 
 fn get_impl<E, X: KeyExtractor<E>>(
@@ -178,8 +158,8 @@ impl<E, X: KeyExtractor<E>> FlatMap<E, X> {
         FlatMap(do_dedup::<E, X>(vec), std::marker::PhantomData)
     }
     pub fn edit(&mut self) -> FlatMapEditGuard<'_, E, X> {
-        let sorted_until = self.0.len();
-        FlatMapEditGuard { src: &mut self.0, sorted_until, marker: self.1 }
+        let real_len = self.0.len();
+        FlatMapEditGuard { src: &mut self.0, real_len, marker: self.1 }
     }
     collection_methods!(0);
     pub fn get<'a>(&'a self, key: &X::KeyBorrowed) -> Option<&'a E> {
@@ -205,25 +185,56 @@ impl<E, X: KeyExtractor<E>> FlatMap<E, X> {
 
 impl<E, X: KeyExtractor<E>> FlatMapEditGuard<'_, E, X> {
     collection_methods!(src);
+
+    /// Return a slice of all the elements in the `Vec`, sorted and otherwise.
+    pub fn as_slice(&self) -> &[E] {
+        let ptr = self.src.as_ptr();
+        unsafe { std::slice::from_raw_parts(ptr, self.real_len) }
+    }
+    /// Return the index of a given element in the full `Vec`.
+    fn get_idx(&self, key: &X::KeyBorrowed) -> Option<usize> {
+        let sorted_until = self.src.len();
+        get_impl::<E, X>(self.as_slice(), sorted_until, key)
+    }
     pub fn get<'a>(&'a self, key: &X::KeyBorrowed) -> Option<&'a E> {
-        let idx = get_impl::<E, X>(self.src.as_slice(), self.sorted_until, key)?;
+        let idx = self.get_idx(key)?;
         Some(unsafe { self.src.get_unchecked(idx) })
     }
     pub fn get_mut<'a>(&'a mut self, key: &X::KeyBorrowed) -> Option<&'a mut E> {
-        let idx = get_impl::<E, X>(self.src.as_slice(), self.sorted_until, key)?;
+        let idx = self.get_idx(key)?;
         Some(unsafe { self.src.get_unchecked_mut(idx) })
     }
     fn push(&mut self, elem: E) -> &mut E {
         let key = X::extract_key(&elem);
-        if self.sorted_until == self.src.len() && Some(key) > self.src.last().map(X::extract_key) {
-            self.sorted_until += 1;
+        let mut sorted_until = self.src.len();
+        let is_sorted =
+            sorted_until == self.real_len && Some(key) > self.as_slice().last().map(X::extract_key);
+        // Pushing to the end of a vector can panic.
+        // We're going to try and ensure that can't happen
+        // while the vector length is set to its true length.
+        // That way, if allocation fails, at least we won't leave the FlatMap in an invalid state.
+        unsafe { self.src.set_len(self.real_len) };
+        match self.src.try_reserve(1) {
+            Ok(()) => {
+                self.src.push(elem);
+                self.real_len += 1;
+                sorted_until += is_sorted as usize;
+            }
+            Err(e) => {
+                unsafe { self.src.set_len(sorted_until) };
+                panic!("FlatMap failed allocation: {e}");
+            }
         }
-        self.src.push(elem);
-        unsafe { self.src.last_mut().unwrap_unchecked() }
+        unsafe { self.src.set_len(sorted_until) };
+        let mut ptr = self.src.as_mut_ptr();
+        unsafe {
+            ptr = ptr.add(self.real_len);
+            ptr.as_mut().unwrap_unchecked()
+        }
     }
     pub fn insert(&mut self, elem: E) -> Option<E> {
         let kb = X::extract_key(&elem).borrow();
-        let idx = get_impl::<E, X>(self.src.as_slice(), self.sorted_until, kb);
+        let idx = self.get_idx(kb);
         if let Some(idx) = idx {
             let old_elem = unsafe { &mut self.src.get_unchecked_mut(idx) };
             Some(std::mem::replace(old_elem, elem))
@@ -234,7 +245,7 @@ impl<E, X: KeyExtractor<E>> FlatMapEditGuard<'_, E, X> {
     }
     pub fn try_insert(&mut self, elem: E) -> Option<E> {
         let kb = X::extract_key(&elem).borrow();
-        if get_impl::<E, X>(self.src.as_slice(), self.sorted_until, kb).is_some() {
+        if self.get_idx(kb).is_some() {
             Some(elem)
         } else {
             self.push(elem);
@@ -245,7 +256,7 @@ impl<E, X: KeyExtractor<E>> FlatMapEditGuard<'_, E, X> {
     /// Mutation of this element can violate the ordering invariant.
     pub fn get_or_insert(&mut self, elem: E) -> (&mut E, Option<E>) {
         let kb = X::extract_key(&elem).borrow();
-        let idx = get_impl::<E, X>(self.src.as_slice(), self.sorted_until, kb);
+        let idx = self.get_idx(kb);
         if let Some(idx) = idx {
             (unsafe { self.src.get_unchecked_mut(idx) }, Some(elem))
         } else {
@@ -256,14 +267,16 @@ impl<E, X: KeyExtractor<E>> FlatMapEditGuard<'_, E, X> {
         // Given we swap_remove, this can absolutely ruin lookup performance.
         // That said, removal should be infrequent, so it's probably
         // not worth adding some sort of tombstoning to the edit guard.
-        let idx = get_impl::<E, X>(self.src.as_slice(), self.sorted_until, key)?;
-        self.sorted_until = idx;
-        Some(self.src.swap_remove(idx))
+        let idx = self.get_idx(key)?;
+        let retval = self.src.swap_remove(idx);
+        self.real_len -= 1;
+        unsafe { self.src.set_len(idx) };
+        Some(retval)
     }
     /// Removes all key-value pairs.
     pub fn clear(&mut self) {
         self.src.clear();
-        self.sorted_until = 0;
+        self.real_len = 0;
     }
 }
 
@@ -273,24 +286,4 @@ impl<E, X: KeyExtractor<E>> FromIterator<E> for FlatMap<E, X> {
     }
 }
 
-impl<E, X: KeyExtractor<E>> Extend<E> for FlatMapEditGuard<'_, E, X> {
-    fn extend<T: IntoIterator<Item = E>>(&mut self, iter: T) {
-        let iter = iter.into_iter();
-        self.src.reserve(iter.size_hint().0);
-        for elem in iter {
-            self.insert(elem);
-        }
-    }
-
-    // Unstable.
-    /*
-    fn extend_one(&mut self, item: (K, V)) {
-        let (key, value) = item;
-        self.insert(item, value);
-    }
-
-    fn extend_reserve(&mut self, additional: usize) {
-        self.pairs.reserve(additional)
-    }
-    */
-}
+// There was an Extend impl here, but it's not used by anything and needed big changes.
--- src/util/tests.rs
+++ src/util/tests.rs
@@ -1,3 +1,5 @@
+use super::FlatMap;
+
 #[test]
 fn thinarc_basic() {
     use super::ThinArc;
@@ -5,4 +7,61 @@ fn thinarc_basic() {
     let sarc2 = sarc1.clone();
     assert!(sarc1.try_unwrap().is_err());
     assert_eq!(sarc2.try_unwrap().ok(), Some(5i32));
+}
+
+#[test]
+fn flatmap_dedup() {
+    use super::do_dedup;
+    let testcases = [
+        (vec![], [].as_slice()),
+        (vec![(1, 0)], &[(1, 0)]),
+        (vec![(1, 0), (2, 0)], &[(1, 0), (2, 0)]),
+        (vec![(1, 0), (2, 0), (3, 0)], &[(1, 0), (2, 0), (3, 0)]),
+        (vec![(1, 0), (2, 0), (2, 0)], &[(1, 0), (2, 0)]),
+        (vec![(1, 0), (1, 0), (2, 0)], &[(1, 0), (2, 0)]),
+        (vec![(1, 0), (2, 0), (2, 0), (3, 0)], &[(1, 0), (2, 0), (3, 0)]),
+        (vec![(1, 0), (2, 0), (2, 1), (2, 2), (3, 0)], &[(1, 0), (2, 2), (3, 0)]),
+    ];
+    for (init, expected) in testcases {
+        let result = do_dedup::<_, ()>(init);
+        assert_eq!(&result, expected);
+    }
+    // Simple test to hopefully catch UAFs.
+    let vec1 = do_dedup::<_, ()>(vec![
+        (1, String::from("foo")),
+        (1, String::from("bar")),
+        (2, String::from("baz")),
+    ]);
+    let vec2 = do_dedup::<_, ()>(vec![
+        (1, String::from("bar")),
+        (2, String::from("foo")),
+        (2, String::from("baz")),
+    ]);
+    assert_eq!(vec1, vec2);
+}
+
+#[test]
+fn flatmap_guard_insert() {
+    let mut map = FlatMap::<(u8, u8)>::from_vec(vec![(0, b'a'), (1, b'b'), (2, b'c')]);
+    let mut guard = map.edit();
+    for value in b'd'..=b'z' {
+        guard.insert((value - b'a', value));
+    }
+    std::mem::drop(guard);
+    assert_eq!(map.len(), 26);
+    for expect in b'a'..=b'z' {
+        let pair = map.get(&(expect - b'a')).expect("missing expected value {expect}");
+        assert_eq!(pair.1, expect);
+    }
+}
+
+#[test]
+fn flatmap_guard_forget() {
+    let mut map = FlatMap::<(u32, char)>::from_vec(vec![(1, 'a'), (2, 'b'), (3, 'c')]);
+    let mut guard = map.edit();
+    guard.insert((4, 'd'));
+    // Remove swap-removes, leaving the the map unsorted after the element that was removed.
+    guard.remove(&3).unwrap();
+    std::mem::forget(guard);
+    assert_eq!(map.len(), 2);
 }
